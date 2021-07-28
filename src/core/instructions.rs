db04fa952283a4491b21f330412941e77f4ae343
Maik Allgöwer <maik@llgoewer.de>
2021-07-28 20:51:03 +0200

    Restructure instruction parsing

--- src/core/instructions.rs
+++ src/core/instructions.rs
@@ -5,64 +5,59 @@ use Instruction::*;
 #[derive(Debug, PartialEq, Eq)]
 pub struct Register(pub(crate) u8);
 
-impl From<u16> for Register {
-    fn from(val: u16) -> Self {
-        Self((val & 0x000F) as u8)
+impl From<u8> for Register {
+    fn from(val: u8) -> Self {
+        Self(val & 0x0F)
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct RegisterRange(pub(crate) u8);
 
-impl From<u16> for RegisterRange {
-    fn from(val: u16) -> Self {
-        Self((val & 0x000F) as u8)
+impl From<u8> for RegisterRange {
+    fn from(val: u8) -> Self {
+        Self(val & 0x0F)
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Address(pub(crate) u16);
 
-impl From<u16> for Address {
-    fn from(val: u16) -> Self {
-        Self(val & 0x0FFF)
+impl From<(u8, u8, u8)> for Address {
+    fn from(val: (u8, u8, u8)) -> Self {
+        Self(
+            ((val.0 as u16) << 8) & 0xF00 |
+            ((val.1 as u16) << 4) & 0x0F0  |
+            (val.2 as u16) & 0x00F
+        )
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Value8(pub(crate) u8);
 
-impl From<u16> for Value8 {
-    fn from(val: u16) -> Self {
-        Self((val & 0x00FF) as u8)
+impl From<(u8, u8)> for Value8 {
+    fn from(val: (u8, u8)) -> Self {
+        Self((val.0 << 4) | (val.1 & 0x0F))
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Value4(pub(crate) u8);
 
-impl From<u16> for Value4 {
-    fn from(val: u16) -> Self {
-        Self((val & 0x000F) as u8)
+impl From<u8> for Value4 {
+    fn from(val: u8) -> Self {
+        Self(val & 0x0F)
     }
 }
 
-trait Nibble {
-    type Output;
-    fn nibbles(&self) -> Self::Output;
-}
-
-impl Nibble for u16 {
-    type Output = (u16, u16, u16, u16);
-
-    fn nibbles(&self) -> Self::Output {
-        (
-            (*self >> 12) & 0xF,
-            (*self >> 8) & 0xF,
-            (*self >> 4) & 0xF,
-            *self & 0xF,
-        )
-    }
+fn nibbles(val: u16) -> (u8, u8, u8, u8) {
+    (
+        (val >> 12) as u8,
+        (val >> 8) as u8,
+        (val >> 4) as u8,
+        val as u8,
+    )
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -105,108 +100,64 @@ pub enum Instruction {
 }
 
 impl Instruction {
-    fn decode_0(ins: u16) -> Result<Self, Error> {
-        match ins {
-            0x00E0 => Ok(I00E0),
-            0x00EE => Ok(I00EE),
-            0x0200..=0x0FFF => Ok(I0NNN(Address::from(ins))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_0(nnn: Address) -> Result<Self, ()> {
+        match nnn {
+            Address(0x00E0) => Ok(I00E0),
+            Address(0x00EE) => Ok(I00EE),
+            Address(0x0200..=0x0FFF) => Ok(I0NNN(nnn)),
+            _ => Err(()),
         }
     }
 
-    fn decode_1(ins: u16) -> Result<Self, Error> {
-        Ok(I1NNN(Address::from(ins)))
-    }
-
-    fn decode_2(ins: u16) -> Result<Self, Error> {
-        Ok(I2NNN(Address::from(ins)))
-    }
-
-    fn decode_3(ins: u16) -> Result<Self, Error> {
-        Ok(I3XNN(Register::from(ins >> 8), Value8::from(ins)))
-    }
-
-    fn decode_4(ins: u16) -> Result<Self, Error> {
-        Ok(I4XNN(Register::from(ins >> 8), Value8::from(ins)))
-    }
-
-    fn decode_5(ins: u16) -> Result<Self, Error> {
-        match ins.nibbles() {
-            (0x5, x, y, 0x0) => Ok(I5XY0(Register::from(x), Register::from(y))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_5(x: Register, y: Register, v: Value4) -> Result<Self, ()> {
+        match v { 
+            Value4(0) => Ok(I5XY0(x, y)),
+            _ => Err(()),
         }
     }
 
-    fn decode_6(ins: u16) -> Result<Self, Error> {
-        Ok(I6XNN(Register::from(ins >> 8), Value8::from(ins)))
-    }
-
-    fn decode_7(ins: u16) -> Result<Self, Error> {
-        Ok(I7XNN(Register::from(ins >> 8), Value8::from(ins)))
-    }
-
-    fn decode_8(ins: u16) -> Result<Self, Error> {
-        match ins.nibbles() {
-            (8, x, y, 0x0) => Ok(I8XY0(Register::from(x), Register::from(y))),
-            (8, x, y, 0x1) => Ok(I8XY1(Register::from(x), Register::from(y))),
-            (8, x, y, 0x2) => Ok(I8XY2(Register::from(x), Register::from(y))),
-            (8, x, y, 0x3) => Ok(I8XY3(Register::from(x), Register::from(y))),
-            (8, x, y, 0x4) => Ok(I8XY4(Register::from(x), Register::from(y))),
-            (8, x, y, 0x5) => Ok(I8XY5(Register::from(x), Register::from(y))),
-            (8, x, y, 0x6) => Ok(I8XY6(Register::from(x), Register::from(y))),
-            (8, x, y, 0x7) => Ok(I8XY7(Register::from(x), Register::from(y))),
-            (8, x, y, 0xE) => Ok(I8XYE(Register::from(x), Register::from(y))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_8(x: Register, y: Register, v: Value4) -> Result<Self, ()> {
+        match v {
+            Value4(0x0) => Ok(I8XY0(x, y)),
+            Value4(0x1) => Ok(I8XY1(x, y)),
+            Value4(0x2) => Ok(I8XY2(x, y)),
+            Value4(0x3) => Ok(I8XY3(x, y)),
+            Value4(0x4) => Ok(I8XY4(x, y)),
+            Value4(0x5) => Ok(I8XY5(x, y)),
+            Value4(0x6) => Ok(I8XY6(x, y)),
+            Value4(0x7) => Ok(I8XY7(x, y)),
+            Value4(0xE) => Ok(I8XYE(x, y)),
+            _ => Err(()),
         }
     }
 
-    fn decode_9(ins: u16) -> Result<Self, Error> {
-        match ins.nibbles() {
-            (9, x, y, 0) => Ok(I9XY0(Register::from(x), Register::from(y))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_9(x: Register, y: Register, n: Value4) -> Result<Self, ()> {
+        match n {
+            Value4(0) => Ok(I9XY0(x, y)),
+            _ => Err(()),
         }
     }
 
-    fn decode_a(ins: u16) -> Result<Self, Error> {
-        Ok(IANNN(Address::from(ins)))
-    }
-
-    fn decode_b(ins: u16) -> Result<Self, Error> {
-        Ok(IBNNN(Address::from(ins)))
-    }
-
-    fn decode_c(ins: u16) -> Result<Self, Error> {
-        Ok(ICXNN(Register::from(ins >> 8), Value8::from(ins)))
-    }
-
-    fn decode_d(ins: u16) -> Result<Self, Error> {
-        Ok(IDXYN(
-            Register::from(ins >> 8),
-            Register::from(ins >> 4),
-            Value4::from(ins),
-        ))
-    }
-
-    fn decode_e(ins: u16) -> Result<Self, Error> {
-        match ins.nibbles() {
-            (0xE, x, 0x9, 0xE) => Ok(IEX9E(Register::from(x))),
-            (0xE, x, 0xA, 0x1) => Ok(IEXA1(Register::from(x))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_e(x: Register, vv: Value8) -> Result<Self, ()> {
+        match vv {
+            Value8(0x9E) => Ok(IEX9E(x)),
+            Value8(0xA1) => Ok(IEXA1(x)),
+            _ => Err(()),
         }
     }
 
-    fn decode_f(ins: u16) -> Result<Self, Error> {
-        match ins.nibbles() {
-            (0xF, x, 0x0, 0x7) => Ok(IFX07(Register::from(x))),
-            (0xF, x, 0x0, 0xA) => Ok(IFX0A(Register::from(x))),
-            (0xF, x, 0x1, 0x5) => Ok(IFX15(Register::from(x))),
-            (0xF, x, 0x1, 0x8) => Ok(IFX18(Register::from(x))),
-            (0xF, x, 0x1, 0xE) => Ok(IFX1E(Register::from(x))),
-            (0xF, x, 0x2, 0x9) => Ok(IFX29(Register::from(x))),
-            (0xF, x, 0x3, 0x3) => Ok(IFX33(Register::from(x))),
-            (0xF, x, 0x5, 0x5) => Ok(IFX55(RegisterRange::from(x))),
-            (0xF, x, 0x6, 0x5) => Ok(IFX65(RegisterRange::from(x))),
-            _ => Err(Error::InvalidInstruction(ins)),
+    fn decode_f(x: Register, vv: Value8) -> Result<Self, ()> {
+        match vv {
+            Value8(0x07) => Ok(IFX07(x)),
+            Value8(0x0A) => Ok(IFX0A(x)),
+            Value8(0x15) => Ok(IFX15(x)),
+            Value8(0x18) => Ok(IFX18(x)),
+            Value8(0x1E) => Ok(IFX1E(x)),
+            Value8(0x29) => Ok(IFX29(x)),
+            Value8(0x33) => Ok(IFX33(x)),
+            Value8(0x55) => Ok(IFX55(x.0.into())),
+            Value8(0x65) => Ok(IFX65(x.0.into())),
+            _ => Err(()),
         }
     }
 }
@@ -216,24 +167,27 @@ impl TryFrom<&[u8]> for Instruction {
 
     fn try_from(instruction: &[u8]) -> Result<Self, Error> {
         let ins = u16::from_be_bytes(instruction[0..2].try_into()?);
-        match ins {
-            0x0000..=0x0FFF => Self::decode_0(ins),
-            0x1000..=0x1FFF => Self::decode_1(ins),
-            0x2000..=0x2FFF => Self::decode_2(ins),
-            0x3000..=0x3FFF => Self::decode_3(ins),
-            0x4000..=0x4FFF => Self::decode_4(ins),
-            0x5000..=0x5FFF => Self::decode_5(ins),
-            0x6000..=0x6FFF => Self::decode_6(ins),
-            0x7000..=0x7FFF => Self::decode_7(ins),
-            0x8000..=0x8FFF => Self::decode_8(ins),
-            0x9000..=0x9FFF => Self::decode_9(ins),
-            0xA000..=0xAFFF => Self::decode_a(ins),
-            0xB000..=0xBFFF => Self::decode_b(ins),
-            0xC000..=0xCFFF => Self::decode_c(ins),
-            0xd000..=0xDFFF => Self::decode_d(ins),
-            0xE000..=0xEFFF => Self::decode_e(ins),
-            0xF000..=0xFFFF => Self::decode_f(ins),
-        }
+        let decoded = match nibbles(ins) {
+            (0x0, a, b, c) => Self::decode_0((a, b, c).into()),
+            (0x1, a, b, c) => Ok(I1NNN((a, b, c).into())),
+            (0x2, a, b, c) => Ok(I2NNN((a, b, c).into())),
+            (0x3, a, b, c) => Ok(I3XNN(a.into(), (b, c).into())),
+            (0x4, a, b, c) => Ok(I4XNN(a.into(), (b, c).into())),
+            (0x5, a, b, c) => Self::decode_5(a.into(), b.into(), c.into()),
+            (0x6, a, b, c) => Ok(I6XNN(a.into(), (b, c).into())),
+            (0x7, a, b, c) => Ok(I7XNN(a.into(), (b, c).into())),
+            (0x8, a, b, c) => Self::decode_8(a.into(), b.into(), c.into()),
+            (0x9, a, b, c) => Self::decode_9(a.into(), b.into(), c.into()),
+            (0xA, a, b, c) => Ok(IANNN((a, b, c).into())),
+            (0xB, a, b, c) => Ok(IBNNN((a, b, c).into())),
+            (0xC, a, b, c) => Ok(ICXNN(a.into(), (b, c).into())),
+            (0xD, a, b, c) => Ok(IDXYN(a.into(), b.into(), c.into())),
+            (0xE, a, b, c) => Self::decode_e(a.into(), (b, c).into()),
+            (0xF, a, b, c) => Self::decode_f(a.into(), (b, c).into()),
+            _ => Err(()),
+        };
+
+        decoded.map_err(|_| Error::InvalidInstruction(ins))
     }
 }
 
@@ -253,6 +207,36 @@ mod tests {
         ( $upper:expr, $lower:expr, $rhs:expr ) => {
             assert_eq!(Instruction::try_from([$upper, $lower].as_ref()), Err($rhs));
         };
+    }
+
+    #[test]
+    fn address() {
+        assert_eq!(Address::from((0x0A, 0x0B, 0x0C)), Address(0xABC));
+        assert_eq!(Address::from((0x1A, 0x2B, 0x4C)), Address(0xABC));
+    }
+
+    #[test]
+    fn register() {
+        assert_eq!(Register::from(0x0A), Register(0x0A));
+        assert_eq!(Register::from(0x1A), Register(0x0A));
+    }
+
+    #[test]
+    fn register_range() {
+        assert_eq!(RegisterRange::from(0x0A), RegisterRange(0x0A));
+        assert_eq!(RegisterRange::from(0x1A), RegisterRange(0x0A));
+    }
+
+    #[test]
+    fn value8() {
+        assert_eq!(Value8::from((0x0A, 0x0B)), Value8(0xAB));
+        assert_eq!(Value8::from((0x1A, 0x2B)), Value8(0xAB));
+    }
+
+    #[test]
+    fn value4() {
+        assert_eq!(Value4::from(0x0A), Value4(0x0A));
+        assert_eq!(Value4::from(0x1A), Value4(0x0A));
     }
 
     #[test]

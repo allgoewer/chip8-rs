6b282368b6ebab2b6021a5d0c624c9ca8796cf1c
Maik Allgöwer <maik@llgoewer.de>
2021-10-24 20:46:14 +0200

    Fix load_program()
    read_exact() errors if the input is too small. This was a bug.

--- chip8_tools/src/util.rs
+++ chip8_tools/src/util.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 pub fn load_program<P: AsRef<Path>>(path: P, target: &mut [u8]) -> io::Result<()> {
     let mut rom = std::fs::File::open(path.as_ref())?;
-    rom.read_exact(&mut target[0x200..])?;
+    rom.read(&mut target[0x200..])?;
 
     Ok(())
 }
